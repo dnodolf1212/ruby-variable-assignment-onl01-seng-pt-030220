16de0c9cde1b7c88d3221cf415bda4cdaac9b8a8
Dwight Nodolf <dnodolf@gmail.com>
2020-03-04 20:34:08 +0000

    Automatically backed up by Learn

--- variable.rb
+++ variable.rb
@@ -1,11 +1,5 @@
  # Define a local variable called 'greeting',
  # give it the value of the string "Hello World"
-filter_parameter_logging
-filter_parameter_logging
-filter_parameter_logging
-filter_parameter_logging
-filter_parameter_logging
-filter_parameter_logging
 greeting = "Hello World"
 
 
